689ad611d522e930abfe85f6e904c83f4b28de9e
Chong Hui <iamchoonggg@gmail.com>
2020-06-01 08:58:10 +0800

    update webpack/babel + add babelrc

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
       },
